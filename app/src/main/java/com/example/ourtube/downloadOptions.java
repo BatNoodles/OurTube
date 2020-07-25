b4aeafef90a3adf5e086abe5512f1af19be34da2
BatNoodles <braeden.windle@gmail.com>
2020-07-26 01:38:42 +1200

    Added download options activity

---
+++ app/src/main/java/com/example/ourtube/downloadOptions.java
@@ -0,0 +1,14 @@
+package com.example.ourtube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class downloadOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_options);
+    }
+}
