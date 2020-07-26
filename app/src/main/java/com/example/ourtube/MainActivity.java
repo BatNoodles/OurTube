f1858cdc284f4c47a7b6038b05839bee062a5777
BatNoodles <braeden.windle@gmail.com>
2020-07-26 15:38:51 +1200

    Changed launcher icon

--- app/src/main/java/com/example/ourtube/MainActivity.java
+++ app/src/main/java/com/example/ourtube/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ourtube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button ytLinkBtn = findViewById(R.id.ytLinkButton);
+        final Button ytSearchBtn = findViewById(R.id.ytSearchButton);
+
+        ytLinkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeIntent = new Intent(MainActivity.this, YoutubeLink.class);
+                startActivity(changeIntent);
+            }
+        });
+
+        ytSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changeIntent = new Intent(MainActivity.this, YoutubeSearch.class);
+                startActivity(changeIntent);
+            }
+        });
+
     }
 }
