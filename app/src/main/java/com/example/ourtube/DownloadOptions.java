eb0420ea38b10b3978670786728f7621447871e0
BatNoodles <braeden.windle@gmail.com>
2020-07-26 18:58:36 +1200

    Buttons dynamically created
    Buttons are now dynamically created for each quality of audio and video (except webm as it doesn't work with the video player on my phone so Im not going to bother working on it).

--- app/src/main/java/com/example/ourtube/DownloadOptions.java
+++ app/src/main/java/com/example/ourtube/DownloadOptions.java
@@ -2,14 +2,18 @@ package com.example.ourtube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DownloadManager;
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import at.huber.youtubeExtractor.VideoMeta;
 import at.huber.youtubeExtractor.YouTubeExtractor;
@@ -17,6 +21,23 @@ import at.huber.youtubeExtractor.YtFile;
 
 public class DownloadOptions extends AppCompatActivity {
 
+    public void addButton(final YtFile file){
+        String btnText = file.getFormat().getExt() + " ";
+        Button btn = new Button(this);
+        btnText += (file.getFormat().getHeight() == -1) ? file.getFormat().getAudioBitrate() + "kb/s" : file.getFormat().getHeight() + "p";
+        btn.setText(btnText);
+
+        final LinearLayout recyclerView = findViewById(R.id.buttonLinearLayout);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(file.getFormat().getExt() + " pressed");
+            }
+        });
+
+        recyclerView.addView(btn);
+    }
 
     public void downloadVideo(String url){
         new YouTubeExtractor(DownloadOptions.this){
@@ -26,37 +47,60 @@ public class DownloadOptions extends AppCompatActivity {
 
                 if (ytFiles != null){
                     YtFile file = null;
+                    List<Integer> videoFormats = new ArrayList<>();
+                    List<Integer> videoItags = new ArrayList<>();
+                    List<Integer> audioFormats = new ArrayList<>();
+                    List<Integer> audioItags = new ArrayList<>();
+
+                    final TextView titleText = findViewById(R.id.videoTitleView);
+                    final ProgressBar progressBar = findViewById(R.id.progressBar);
                     for(int i=0, itag; i < ytFiles.size(); i++){
                         itag = ytFiles.keyAt(i);
                         YtFile tempFile =  ytFiles.get(itag);
-                        System.out.println(tempFile.getFormat().getExt());
-                        if (tempFile.getFormat().getExt().equals("m4a")){
-                            file = tempFile;
-                            System.out.println("file has been found");
+                        if (tempFile.getFormat().getHeight() != -1){
+                            if (!videoFormats.contains(tempFile.getFormat().getHeight())){
+                                videoFormats.add(tempFile.getFormat().getHeight());
+                                videoItags.add(itag);
+                            }
                         }
-                    }
+                        else{
+                            if (!audioFormats.contains(tempFile.getFormat().getHeight()) && !tempFile.getFormat().getExt().equals("webm")){
+                                audioFormats.add(tempFile.getFormat().getHeight());
+                                audioItags.add(itag);
 
-                    if (file != null){
-                        String downloadUrl = file.getUrl();
-                        String title = videoMeta.getTitle();
-                        String filename = title + "." + file.getFormat().getExt();
-                        Toast myToast = Toast.makeText(DownloadOptions.this, "Downloading " + filename, Toast.LENGTH_LONG);
-                        myToast.show();
-
-                        Uri uri = Uri.parse(downloadUrl);
-
-                        DownloadManager.Request request = new DownloadManager.Request(uri);
-
-                        request.allowScanningByMediaScanner();
-                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "OurTube/" + filename);
-
-                        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-
-                        manager.enqueue(request);
-                        finish();
+                            }
+                        }
 
                     }
+                    for (int i = 0; i < videoItags.size(); i++){
+                        addButton(ytFiles.get(videoItags.get(i)));
+                    }
+                    for (int i = 0; i < audioItags.size(); i++){
+                        addButton((ytFiles.get(audioItags.get(i))));
+                    }
+                    titleText.setText("File formats for: " + videoMeta.getTitle());
+                    progressBar.setVisibility(View.GONE);
+//                    if (file != null){
+//                        String downloadUrl = file.getUrl();
+//                        String title = videoMeta.getTitle();
+//                        String filename = title + "." + file.getFormat().getExt();
+//                        Toast myToast = Toast.makeText(DownloadOptions.this, "Downloading " + filename, Toast.LENGTH_LONG);
+//                        myToast.show();
+//
+//                        Uri uri = Uri.parse(downloadUrl);
+//
+//                        DownloadManager.Request request = new DownloadManager.Request(uri);
+//
+//                        request.allowScanningByMediaScanner();
+//                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+//                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "OurTube/" + filename);
+//
+//                        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+//
+//                        manager.enqueue(request);
+//                        finish();
+//
+//                    }
                 }
                 else{
                     Toast myToast = Toast.makeText(DownloadOptions.this, "That link is invalid", Toast.LENGTH_SHORT);
