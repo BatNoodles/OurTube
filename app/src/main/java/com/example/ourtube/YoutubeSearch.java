9cbde154c3604c6de06ca3df169ea807db8c625a
BatNoodles <braeden.windle@gmail.com>
2020-07-26 19:38:28 +1200

    Tidied up
    Make the buttons on the main activity black so they stand out more. Also added a toast for when the user tries to download based off of a search term telling them that it isnt working yet

--- app/src/main/java/com/example/ourtube/YoutubeSearch.java
+++ app/src/main/java/com/example/ourtube/YoutubeSearch.java
@@ -3,6 +3,9 @@ package com.example.ourtube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class YoutubeSearch extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class YoutubeSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_search);
+
+        final Button downloadButton = findViewById(R.id.downloadButton);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(YoutubeSearch.this,  "This does not work yet", Toast.LENGTH_SHORT);
+                myToast.show();
+                finish();
+            }
+        });
     }
 }
